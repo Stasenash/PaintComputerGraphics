81d8c56749296167e7640441914eda3efa20615b
Stasenash <staslisenash@gmail.com>
2021-12-10 21:14:44 +0500

    reformat files

--- Paint/ClipboardTool.cs
+++ Paint/ClipboardTool.cs
@@ -7,90 +7,109 @@ using System.Drawing.Imaging;
 
 namespace Paint
 {
-  public class ClipboardTool : RectangleToolBase
-  {
-    private ClipboardAction action;
-    private Rectangle prevRect;
-    private Rectangle rect;
-    private Pen delPen;
-    private Pen pen;
-    private Point curPoint;
+    public class ClipboardTool : RectangleToolBase
+    {
+        private ClipboardAction action;
+        private Rectangle prevRect;
+        private Rectangle rect;
+        private Pen delPen;
+        private Pen pen;
+        private Point curPoint;
 
-    public ClipboardTool(ToolArgs args, ClipboardAction action)
-      : base(args) {
-      this.action = action;
-      args.pictureBox.MouseClick += new MouseEventHandler(OnMouseClick);
-    }
-
-    private void OnMouseClick(object sender, MouseEventArgs e) {
-      if (e.Button == MouseButtons.Right) {
-        if (Clipboard.ContainsImage()) {
-          PasteImage(curPoint);
-          args.pictureBox.Invalidate();
+        public ClipboardTool(ToolArgs args, ClipboardAction action)
+          : base(args)
+        {
+            this.action = action;
+            args.pictureBox.MouseClick += new MouseEventHandler(OnMouseClick);
         }
-      }
-    }
 
-    protected override void OnMouseDown(object sender, MouseEventArgs e) {
-      if (e.Button == MouseButtons.Left) {
-        drawing = true;
-        sPoint = e.Location;
-        g = args.pictureBox.CreateGraphics();
-        pen = Pens.Black;
-        delPen = new Pen(new TextureBrush(args.bitmap), 1);
-      }
-    }
-
-    protected override void OnMouseMove(object sender, MouseEventArgs e) {
-      curPoint = e.Location;
-      if (drawing) {
-        // delete old
-        g.DrawRectangle(delPen, prevRect);
-        // draw the new rectangle
-        rect = GetRectangleFromPoints(sPoint, curPoint);
-        g.DrawRectangle(pen, rect);
-
-        prevRect = rect;
-
-        ShowPointInStatusBar(sPoint, e.Location);
-      } else {
-        ShowPointInStatusBar(e.Location);
-      }
-    }
-
-    protected override void OnMouseUp(object sender, MouseEventArgs e) {
-      drawing = false;
-      if (e.Button == MouseButtons.Left) {
-        if ((action == ClipboardAction.Copy) || (action == ClipboardAction.Cut)) {
-          // copy rectangle
-          Bitmap copiedBmp = args.bitmap.Clone(rect, args.bitmap.PixelFormat);
-          Clipboard.SetImage(copiedBmp);
-          if (action == ClipboardAction.Cut) {
-            // delete copied rectangle
-            Graphics g = Graphics.FromImage(args.bitmap);
-            g.FillRectangle(new SolidBrush(args.settings.SecondaryColor), rect);
-          }
-        } else if (action == ClipboardAction.Paste) {
-          if (Clipboard.ContainsImage())
-            PasteImage(rect);
+        private void OnMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    PasteImage(curPoint);
+                    args.pictureBox.Invalidate();
+                }
+            }
         }
-        args.pictureBox.Invalidate();
-      }
-    }
 
-    private void PasteImage(Rectangle rect) {
-      Graphics gc = Graphics.FromImage(args.bitmap);
-      gc.DrawImage(Clipboard.GetImage(), rect);
-    }
+        protected override void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drawing = true;
+                sPoint = e.Location;
+                g = args.pictureBox.CreateGraphics();
+                pen = Pens.Black;
+                delPen = new Pen(new TextureBrush(args.bitmap), 1);
+            }
+        }
 
-    private void PasteImage(Point p) {
-      Graphics gc = Graphics.FromImage(args.bitmap);
-      gc.DrawImage(Clipboard.GetImage(), p);
-    }
+        protected override void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            curPoint = e.Location;
+            if (drawing)
+            {
+                // delete old
+                g.DrawRectangle(delPen, prevRect);
+                // draw the new rectangle
+                rect = GetRectangleFromPoints(sPoint, curPoint);
+                g.DrawRectangle(pen, rect);
 
-    public override void UnloadTool() {
-      base.UnloadTool();
-      args.pictureBox.MouseClick -= new MouseEventHandler(OnMouseClick);
+                prevRect = rect;
+
+                ShowPointInStatusBar(sPoint, e.Location);
+            }
+            else
+            {
+                ShowPointInStatusBar(e.Location);
+            }
+        }
+
+        protected override void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+            if (e.Button == MouseButtons.Left)
+            {
+                if ((action == ClipboardAction.Copy) || (action == ClipboardAction.Cut))
+                {
+                    // copy rectangle
+                    Bitmap copiedBmp = args.bitmap.Clone(rect, args.bitmap.PixelFormat);
+                    Clipboard.SetImage(copiedBmp);
+                    if (action == ClipboardAction.Cut)
+                    {
+                        // delete copied rectangle
+                        Graphics g = Graphics.FromImage(args.bitmap);
+                        g.FillRectangle(new SolidBrush(args.settings.SecondaryColor), rect);
+                    }
+                }
+                else if (action == ClipboardAction.Paste)
+                {
+                    if (Clipboard.ContainsImage())
+                        PasteImage(rect);
+                }
+                args.pictureBox.Invalidate();
+            }
+        }
+
+        private void PasteImage(Rectangle rect)
+        {
+            Graphics gc = Graphics.FromImage(args.bitmap);
+            gc.DrawImage(Clipboard.GetImage(), rect);
+        }
+
+        private void PasteImage(Point p)
+        {
+            Graphics gc = Graphics.FromImage(args.bitmap);
+            gc.DrawImage(Clipboard.GetImage(), p);
+        }
+
+        public override void UnloadTool()
+        {
+            base.UnloadTool();
+            args.pictureBox.MouseClick -= new MouseEventHandler(OnMouseClick);
+        }
     }
-  }
 }
--- Paint/PointerTool.cs
+++ Paint/PointerTool.cs
@@ -6,22 +6,25 @@ using System.Windows.Forms;
 
 namespace Paint
 {
-  public class PointerTool : Tool
-  {
-    public PointerTool(ToolArgs args)
-      : base(args) {
-      args.pictureBox.Cursor = Cursors.Arrow;
-      args.pictureBox.MouseMove += new MouseEventHandler(OnMouseMove);
-    }
+    public class PointerTool : Tool
+    {
+        public PointerTool(ToolArgs args)
+          : base(args)
+        {
+            args.pictureBox.Cursor = Cursors.Arrow;
+            args.pictureBox.MouseMove += new MouseEventHandler(OnMouseMove);
+        }
 
-    private void OnMouseMove(object sender, MouseEventArgs e) {
-      // show cursor location in status bar
-      ShowPointInStatusBar(e.Location);
-    }
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            // show cursor location in status bar
+            ShowPointInStatusBar(e.Location);
+        }
 
-    public override void UnloadTool() {
-      // remove event handlers
-      args.pictureBox.MouseMove -= new MouseEventHandler(OnMouseMove);
+        public override void UnloadTool()
+        {
+            // remove event handlers
+            args.pictureBox.MouseMove -= new MouseEventHandler(OnMouseMove);
+        }
     }
-  }
 }
